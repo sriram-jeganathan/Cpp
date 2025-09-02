af300157192a6279ad4365de88c923f2c75cd8d1
sriram-jeganathan <sriram.jeganathan@outlook.com>
2025-09-03 00:01:56 +0530

    added test

--- test/singly-linked-list/list.c
+++ test/singly-linked-list/list.c
@@ -58,7 +58,10 @@ void reverse ( Node *current, Node *previous ) {
 }
 
 void reverseList ( List *list ) {
-    reverseList ( list->head, NULL );
+    reverse ( list->head, NULL );
+    Node *temp = list->head;
+    list->head = list->tail;
+    list->tail = temp;
     return;
 }
 
@@ -81,7 +84,7 @@ void searchNode ( List *list, int number ) {
         if ( current->data == number ) {
             printf( "%d exists in List\n", number );
             check = 1;
-            return 0;
+            return;
         }
         current = current->next;
     }
@@ -91,17 +94,101 @@ void searchNode ( List *list, int number ) {
     }
 }
 
+void insertHead ( List *list, int x ) {
+    Node *newNode  = malloc ( sizeof ( Node ) );
+    newNode->data = x;
+    newNode->next = NULL;
+
+    if ( list->head == NULL ) {
+        list->head = newNode;
+        list->tail = newNode;
+        return;
+    }
+
+    newNode->next = list->head;
+    list->head = newNode;
+    return;
+}
+
+void insertMiddle ( List *list, int pos, int x ) {
+    Node *newNode = malloc ( sizeof ( Node ) );
+    newNode->data = x;
+    newNode->next = NULL;
+    int count = 0;
+
+    Node *current = list->head;
+    while ( NULL != current ) {
+        if ( current->next->data == x ) {
+            Node *temp = current;
+        }
+        if ( count == pos ) {
+            break;
+        }
+        current = current->next;
+        count++;
+    }
+
+    temp->next = newNode;
+    newNode->next = current;
+    return;
+}
+
+void insertTail ( List *list, int x ) {
+    Node *newNode = malloc ( sizeof ( Node ) );
+    newNode->data = x;
+    newNode->next = NULL;
+
+    list->tail->next = newNode;
+    list->tail = newNode;
+    return;
+}
+
 void deleteNode ( List *list, int number ) {
     Node *current = list->head;
     while ( current != NULL ) {
+        if ( current->next->data == number ) {
+            Node *temp = current;
+        }
         if ( current->data == number ) {
             break;
         }
         current = current->next;
     }
+    temp->next = current->next;
+    current->data = 0;
+    current->next = NULL;
+    free(current);
+    return;
 }  
+
+void mergeList ( List *list1, List *list2 ) {
+    Node *current = list2->head;
+    while ( NULL != current ) {
+        addNode ( list1, current->data );
+        current = current->next;
+    }
+    return; 
+}
+
+void sort ( List *list ) {
+	Node *outer = list->head;
+	Node *inner = list->head;
+	Node *temp = malloc( sizeof(Node) );
+	
+	while ( NULL != outer ) {
+		inner = list->head;
+		while ( NULL != inner ) {
+			if ( inner->number > outer->number ) {
+				temp->number = inner->number;
+				inner->number = outer->number;
+				outer->number = temp->number;
+			}
+			inner = inner->next;
+		}
+		outer = outer->next;
+	}
+}
 
 void freeList ( List *list ) {
 
 }
-
--- test/singly-linked-list/main.c
+++ test/singly-linked-list/main.c
@@ -2,9 +2,78 @@
 
 int main ( void ) {
     List* list = createList ();
+
+    // adding elements to one linked list
     for ( int i = 10; i < 110; i += 10 ) {
         addNode ( list, i );
     }
+
+    // forward printing
+    printf ( "Forward Print:\n" );
     forwardPrint ( list );
+    printf ( "\n" );
+
+    // backward printing
+    printf ( "Backward Print:\n" );
+    backwardPrint ( list );
+    printf ( "\n" );
+
+    // reversing list
+    printf ( "List Reversal:\n" );
+    reverseList ( list );
+    forwardPrint ( list );
+    printf ( "\n" );
+
+    // counting the number of nodes
+    printf ( "Node Count:\n" );
+    int size = count ( list );
+    printf ( "Size of List: %d\n\n", size );
+
+    // search the given node
+    printf ( "Node Search:\n" );
+    searchNode ( list, 70 );
+    searchNode ( list, 15 );
+    printf ( "\n" );
+
+    // inserting head
+    printf( "Head Insertion:\n" );
+    insertHead ( list, 0 );
+    printf ( "Head changed to 0\n");
+    // inserting tail
+    printf ( "Tail Insertion:\n" );
+    insertTail ( list, 110 );
+    printf ( "Tail changed to 110\n");
+    printf ( "\n" );
+
+    // deleting a node
+    printf ( "Node Deleted:\n" );
+    deleteNode ( list, 0 );
+    forwardPrint ( list );
+    printf ( "Node Deleted:\n" );
+    deleteNode ( list, 110 );
+    fowardPrint ( list );
+    printf ( "\n" );
+
+    // merging 2 lists
+    List *list2 = createList ();
+    for ( int i = 5; i < 110; i+=10 ) {
+        addNode ( list2, i );
+    }
+    printf ( "List 2:\n" );
+    forwardPrint ( list );
+    printf ( "\n" );
+
+    // sorting the 2 lists
+    printf ( "Before sorting:\n" );
+    mergeList ( list, list2 );
+    forwardPrint ( list );
+    printf ( "After sorting:\n" );
+    sort ( list );
+    forwardPrint ( list );
+
     return 0;
 }
+
+/*
+void freeList ( List* );
+*/
